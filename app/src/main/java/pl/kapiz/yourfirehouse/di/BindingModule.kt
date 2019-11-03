d56d9aae204194c1cda1f8736b76c428d67d0dcb
Kacper Ziubryniewicz <kapi2289@gmail.com>
2019-11-03 17:32:18 +0100

    Move fragment bindings to BindingModule

--- app/src/main/java/pl/kapiz/yourfirehouse/di/BindingModule.kt
+++ app/src/main/java/pl/kapiz/yourfirehouse/di/BindingModule.kt
@@ -3,23 +3,42 @@ package pl.kapiz.yourfirehouse.di
 import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import pl.kapiz.yourfirehouse.di.scopes.PerActivity
+import pl.kapiz.yourfirehouse.di.scopes.PerFragment
+import pl.kapiz.yourfirehouse.ui.alarms.AlarmsFragment
+import pl.kapiz.yourfirehouse.ui.home.HomeFragment
 import pl.kapiz.yourfirehouse.ui.login.LoginActivity
 import pl.kapiz.yourfirehouse.ui.main.MainActivity
-import pl.kapiz.yourfirehouse.ui.main.MainModule
 import pl.kapiz.yourfirehouse.ui.splash.SplashActivity
 
 @Module
+@Suppress("unused")
 internal abstract class BindingModule {
+
+    /**
+     * ACTIVITIES
+     */
 
     @PerActivity
     @ContributesAndroidInjector
     abstract fun bindSplashActivity(): SplashActivity
 
     @PerActivity
-    @ContributesAndroidInjector(modules = [MainModule::class])
+    @ContributesAndroidInjector()
     abstract fun bindMainActivity(): MainActivity
 
     @PerActivity
     @ContributesAndroidInjector
     abstract fun bindLoginActivity(): LoginActivity
+
+    /**
+     * FRAGMENTS
+     */
+
+    @PerFragment
+    @ContributesAndroidInjector
+    abstract fun bindHomeFragment(): HomeFragment
+
+    @PerFragment
+    @ContributesAndroidInjector
+    abstract fun bindAlarmsFragment(): AlarmsFragment
 }
